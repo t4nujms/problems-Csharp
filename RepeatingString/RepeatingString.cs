2e01d38d02007bb47fc6f32cce3d83a3efe2b21f
TMS <t4nujms@gmail.com>
2022-08-31 19:39:52 +0100

    Code solution to Repeating String problem

---
+++ RepeatingString/RepeatingString.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace RepeatingString
+{
+    internal class RepeatingString
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string to repeat: ");
+            string? str = Console.ReadLine();
+
+            Console.Write("Enter the number of times to repeat the string: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            StringBuilder display = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                display.Append(str);
+            }
+
+            Console.WriteLine(display.ToString());
+        }
+    }
+}
