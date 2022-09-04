c7e3cf985e8fd92af2d4c6aa939b9abf98381e3b
t4nujms <51231639+t4nujms@users.noreply.github.com>
2022-09-04 17:47:46 +0100

    Permutations of an array problem scaffold

---
+++ ArrayPermutations/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ArrayPermutations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[] { 1, 2, 3 };
+
+            int[] arr2 = new int[] { 1, 2, 3, 4, 5 };
+
+            int[] arr3 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        }
+    }
+}
