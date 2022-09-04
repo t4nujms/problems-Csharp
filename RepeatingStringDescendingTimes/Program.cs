c7825e79fc51566e23db44579d2383a4c4eb7a47
t4nujms <51231639+t4nujms@users.noreply.github.com>
2022-09-04 17:47:16 +0100

    RepeatingStrings problem with descending number of repeats

---
+++ RepeatingStringDescendingTimes/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace RepeatingStringDescendingTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string to repeat: ");
+            string? str = Console.ReadLine();
+
+            Console.Write("Enter the number of times to repeat the string: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = n; i > 0; i--)
+            {
+                string display = string.Empty;
+                for (int j = 0; j < i; j++)
+                {
+                    display = display + str;
+                }
+
+                Console.WriteLine(display);
+            }
+        }
+    }
+}
